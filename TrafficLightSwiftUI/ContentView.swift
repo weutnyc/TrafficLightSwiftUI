9bec58cff46a7959947d3dd8cf7edafb92ee1442
weutnyc <weutnyc@gmail.com>
2021-06-02 20:37:28 +0300

    ready

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -52,7 +52,6 @@ struct ContentView: View {
                 greenLight
             }
             Spacer()
-            
             Button(action: { work() }) {
                 Text("\(buttonText)")
                     .font(.title)
